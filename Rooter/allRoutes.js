628f025615454045c0426b387c7ce5e487681c1d
krazyxav <xavier.migaud@orange.fr>
2026-02-12 07:56:55 +0100

    routage

---
+++ Rooter/allRoutes.js
@@ -0,0 +1,46 @@
+import Route from "./Route.js";
+export { allRoutes };
+
+
+class AllRoutes {
+
+    constructor() {
+        this.routes = [];
+    }
+
+    addRoute(route) {
+        this.routes.push(route);
+    }
+
+    getRouteByUrl(url){
+
+        let routeFound = false;
+
+        this.routes.forEach((route) => {
+            if (route.url == url) {
+                routeFound = route;
+            }
+        });
+
+        if (routeFound) {
+            return routeFound;
+        } else {
+            return this.get404();
+        }
+    }
+
+    get404() {
+        return this.getRouteByUrl("404");
+    }
+
+}
+
+
+const allRoutes = new AllRoutes();
+
+allRoutes.addRoute(new Route("/", "Accueil", "/pages/home.html"));
+allRoutes.addRoute(new Route("404", "Page introuvable", "/pages/404.html"));
+
+export const websiteName = "Quai Antique";
+
+
