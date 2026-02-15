b7236030b9ebe21524cc2c4b6a5e6b0476520bc6
krazyxav <xavier.migaud@orange.fr>
2026-02-15 10:53:48 +0100

    page connexion

--- Rooter/allRoutes.js
+++ Rooter/allRoutes.js
@@ -41,6 +41,8 @@ const allRoutes = new AllRoutes();
 allRoutes.addRoute(new Route("/", "Accueil", "/pages/home.html"));
 allRoutes.addRoute(new Route("404", "Page introuvable", "/pages/404.html"));
 allRoutes.addRoute(new Route("/galerie", "Galerie", "/pages/galerie.html"));
+allRoutes.addRoute(new Route("/menu", "Menu", "/pages/menu.html"));
+allRoutes.addRoute(new Route("/signin", "Conenxion", "/pages/signin.html"));
 
 export const websiteName = "Quai Antique";
 
